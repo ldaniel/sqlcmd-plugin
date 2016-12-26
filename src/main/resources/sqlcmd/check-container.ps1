51a81e3e26748a0ca3a36a74ad92349cb9de14c6
Leandro Daniel <contato@leandrodaniel.com>
2016-12-26 13:41:03 -0200

    Add files via upload

---
+++ src/main/resources/sqlcmd/check-container.ps1
@@ -0,0 +1,32 @@
+$_TempPath = $($container.TempPath)
+
+function ExecuteCheck()
+{
+
+	If (Test-Path $_TempPath)
+	{
+		PrintMessageOk $_TempPath 
+	}
+	else
+	{
+		PrintHowToSolveIssue $_TempPath
+		Exit 1
+	}
+}
+
+function PrintMessageOk($folder)
+{
+	Write-Host "-----------------------------------------------------------------------------------------------------"
+	Write-Host "[OK!]"
+	Write-Host "Folder '$($folder)' exists on server."	
+}
+
+function PrintHowToSolveIssue($folder)
+{
+	Write-Host "-----------------------------------------------------------------------------------------------------"
+	Write-Host "[ERROR!]"
+	Write-Host "Check task failed due the following reason: Folder '$($folder)' does not exists on server."	
+	Write-Host "The folder '$($folder)' must be created on target server in order to solve this issue."	
+}
+
+ExecuteCheck
